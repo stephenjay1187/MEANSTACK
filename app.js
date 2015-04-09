f44f608810293e5798729e0febc35aa2a0dbffb3
Stephen Jacobs <stephenjay1187@gmail.com>
2015-04-08 22:09:31 -0700

    First commit of my test application
    Added: app.js, index.html.  Imported: angular, bootstrap, font-awesome
    and jQuery via CDN.

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = angular.module("flapperNews", []);
+
+app.controller("MainCtrl", [
+"$scope",
+function($scope){
+	
+	$scope.test = "Hello World";
+
+	$scope.posts = [
+		{ title : "post 1", upvotes: 5},
+		{ title : "post 2", upvotes: 8},
+		{ title : "post 3", upvotes: 3},
+		{ title : "post 4", upvotes: 2},
+		{ title : "post 5", upvotes: 55}
+	];
+
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === ""){return;}
+		$scope.posts.push({
+			title: $scope.title, 
+			link: $scope.link,
+			upvotes: 0
+		});
+
+		$scope.title = "";
+		$scope.link = "";
+	};
+
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	};
+
+}]);
